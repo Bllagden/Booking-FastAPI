bb4c9b357081c82b74606b61bcb4e7a6554aed1f
Bllagden <bllagden99@gmail.com>
2024-01-07 23:23:43 +0400

    Tests setup_database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,48 @@
 import asyncio
+import json
+from datetime import datetime
 
 import pytest
+from sqlalchemy import insert
 
-from db import Base, async_engine, async_session_factory  # noqa: F401
-from db.models import Bookings, Hotels, Rooms, Users  # noqa: F401
+from db import Base, async_engine, async_session_factory
+from db.models import Bookings, Hotels, Rooms, Users
 from settings import db_settings
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def setup_db():
+async def setup_database():
     assert db_settings.MODE == "TEST"
 
     async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all())
-        await conn.run_sync(Base.metadata.create_all())
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_json(model: str):
+        with open(f"tests/mock_{model}.json", encoding="utf-8") as file:
+            return json.load(file)
+
+    hotels = open_mock_json("hotels")
+    rooms = open_mock_json("rooms")
+    users = open_mock_json("users")
+    bookings = open_mock_json("bookings")
+
+    # преобразование дат для алхимии (str -> datetime)
+    for booking in bookings:
+        booking["date_from"] = datetime.strptime(booking["date_from"], "%Y-%m-%d")
+        booking["date_to"] = datetime.strptime(booking["date_to"], "%Y-%m-%d")
+
+    async with async_session_factory() as session:
+        for Model, values in [
+            (Hotels, hotels),
+            (Rooms, rooms),
+            (Users, users),
+            (Bookings, bookings),
+        ]:
+            stmt = insert(Model).values(values)
+            await session.execute(stmt)
+
+        await session.commit()
 
 
 # Взято из документации к pytest-asyncio
