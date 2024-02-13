8d3603495fb200b77fe53bca639ad17e4c8f4483
Bllagden <bllagden99@gmail.com>
2024-02-13 23:52:03 +0400

    Add celery-dependencies

--- docker/postgres_waiting.sh
+++ docker/postgres_waiting.sh
@@ -6,9 +6,9 @@ host="$DB_HOST"
 port="$DB_PORT"
 
 until timeout 1 bash -c "echo > /dev/tcp/$host/$port"; do
-  >&2 echo "Postgres is unavailable - Booking sleeping"
+  >&2 echo "Postgres is unavailable -> Booking sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - Booking executing"
+>&2 echo "Postgres is up -> Booking executing"
 exec $@
